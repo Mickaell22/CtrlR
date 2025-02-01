4763d788825fe9763a3f6fdcff3677fa04205741
Mickaell22 <mickaelmoranvera03@gmail.com>
2025-02-01 12:48:15 -0500

    Update data.js

--- js/data.js
+++ js/data.js
@@ -8,25 +8,8 @@ window.rifasData = [
     "numeros": 50,
     "numerosVendidos": {
       "1": "Micka",
-      "2": "moni"
+      "2": "moni",
+      "3": "mick"
     }
-  },
-  {
-    "id": 1738429957871,
-    "titulo": "Gran Rifa de perfume",
-    "premio": "Perfume",
-    "fecha": "2025-02-28",
-    "precio": "1",
-    "numeros": 50,
-    "numerosVendidos": {}
-  },
-  {
-    "id": 1738431970617,
-    "titulo": "Gran Rifa de perfume",
-    "premio": "Perfume 2",
-    "fecha": "2025-02-28",
-    "precio": "1",
-    "numeros": 50,
-    "numerosVendidos": {}
   }
 ];
