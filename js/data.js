a0e00f791301af3e3fc768b67a764ca4f699ba40
Mickaell22 <mickaelmoranvera03@gmail.com>
2025-02-04 21:22:04 -0500

    Rifa #1

---
+++ js/data.js
@@ -0,0 +1,13 @@
+window.rifasData = [
+  {
+    "id": 1738722088912,
+    "titulo": "Gran Rifa de Zapatos",
+    "premio": "Zapatos",
+    "fecha": "2025-02-28",
+    "precio": "1",
+    "imagen1": "img/rifas/rifa_1738722088912_1.png",
+    "imagen2": "img/rifas/rifa_1738722088912_2.jpg",
+    "numeros": 50,
+    "numerosVendidos": {}
+  }
+];
